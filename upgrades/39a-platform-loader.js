bceec54e60ca278e406db7f720645c85109a3dc9
jackq120219 <166075321+jackq120219@users.noreply.github.com>
2026-09-08 13:18:30 -0500

    Add staged platform feature loader

---
+++ upgrades/39a-platform-loader.js
@@ -0,0 +1,6 @@
+'use strict';
+(()=>{
+ const files=['40-readiness-path.js'];let i=0;
+ function next(){if(i>=files.length)return;const s=document.createElement('script');s.src=`/upgrades/${files[i++]}`;s.defer=true;s.onload=next;s.onerror=next;document.head.appendChild(s)}
+ next();
+})();
